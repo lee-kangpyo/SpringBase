161f4ce036b9d6aa855782e9902b384bb42d709c
baedormann <89854178+baedormann@users.noreply.github.com>
2025-06-16 11:55:55 +0900

    시크릿키 .env로 이동 및 새로 생성 스프링 구동 테스트 케이스에 .env 추가

--- src/test/java/com/akmz/springBase/SpringBaseApplicationTests.java
+++ src/test/java/com/akmz/springBase/SpringBaseApplicationTests.java
@@ -1,11 +1,15 @@
 package com.akmz.springBase;
 
+import com.akmz.springBase.base.test.DotenvContextInitializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(initializers = DotenvContextInitializer.class)
 class SpringBaseApplicationTests {
 
+	// 스프링 구동 테스트용 에러 안나면 정상적으로 빌드됨
 	@Test
 	void contextLoads() {
 	}
