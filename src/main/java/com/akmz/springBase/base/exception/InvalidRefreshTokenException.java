724e43022cfcfde69fe76adb572583afe18afdf9
baedormann <89854178+baedormann@users.noreply.github.com>
2025-07-25 16:43:48 +0900

    프로젝트 구조 도메인으로 분리

--- src/main/java/com/akmz/springBase/base/exception/InvalidRefreshTokenException.java
+++
@@ -1,8 +0,0 @@
-package com.akmz.springBase.base.exception;
-
-// 리프레쉬 토큰이 유효하지 않음
-public class InvalidRefreshTokenException extends RuntimeException {
-    public InvalidRefreshTokenException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/akmz/springBase/base/exception/RefreshTokenMismatchException.java
+++
@@ -1,8 +0,0 @@
-package com.akmz.springBase.base.exception;
-
-// 사용자가 전달한 리프레쉬 토큰이 DB에 있는 리프레쉬 토큰과 일치하지 않음
-public class RefreshTokenMismatchException extends RuntimeException {
-  public RefreshTokenMismatchException(String message) {
-    super(message);
-  }
-}
